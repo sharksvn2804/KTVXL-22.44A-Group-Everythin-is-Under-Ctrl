d6c6f7e484ca4d1b384f9d95877448286c9662cf
Trần Quang Khánh <131978279+sharksvn2804@users.noreply.github.com>
2025-12-12 22:04:15 +0700

    Thêm PWM

--- code/main/app_priv.h
+++ code/main/app_priv.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include <stdbool.h>
+#include <driver/ledc.h>
 #include "driver/gpio.h"
 #include "driver/adc.h"
 
@@ -13,12 +14,15 @@
 // ==================== LED ====================
 // GPIO Pins:
 #define GP2Y_LED_POWER    19    // LED của GP2Y dùng để kích hoạt cảm biến bụi (dùng cho GP2Y1010AU0F)
-#define LED_WARNING_PIN   3     // LED đỏ 1 - cảnh báo khi CO hoặc PM2.5 cao
 #define LED_MODE_PIN      2     // LED xanh - chế độ cảnh báo
 #define ALERT_BUTTON_PIN  18    // Nút GPIO18 để toggle alert mode từ button hoặc RainMaker
-// CO Alert Mode:
-#define CO_THRESHOLD      9.5   // ngưỡng cảnh báo CO (ppm)
-#define PM25_THRESHOLD    0.15  // ngưỡng cảnh báo PM2.5 (mg/m³)
+// GPIO cho PWM:
+#define LED_WARNING_PIN   3     // LED đỏ 1 - cảnh báo khi CO hoặc PM2.5
+#define LEDC_CHANNEL      LEDC_CHANNEL_0
+#define LEDC_TIMER        LEDC_TIMER_0
+#define LEDC_MODE         LEDC_LOW_SPEED_MODE
+#define LEDC_DUTY_RES     LEDC_TIMER_8_BIT
+#define LEDC_FREQUENCY    5000  // 5 kHz
 
 // ==================== Functions ====================
 void app_driver_init(void);
