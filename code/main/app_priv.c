edbba2510abf0b9defebb0c8bb7ab847bc0e35b0
Trần Quang Khánh <131978279+sharksvn2804@users.noreply.github.com>
2025-12-13 10:37:17 +0700

    Update app_priv.c

--- code/main/app_priv.c
+++ code/main/app_priv.c
@@ -92,6 +92,16 @@ void app_driver_init(void) {
     gpio_config(&led_cfg);
     gpio_set_level(LED_WARNING_PIN, 0);
     gpio_set_level(LED_MODE_PIN, 0);
+    // Khởi tạo GPIO cho còi:
+    gpio_config_t buzz_cfg = {
+        .pin_bit_mask = (1ULL << BUZZ_PIN),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&buzz_cfg);
+    gpio_set_level(BUZZ_PIN, 0);
     // Khởi tạo LCD (tiêu chí I2C):
     lcd_init();
     lcd_clear();
