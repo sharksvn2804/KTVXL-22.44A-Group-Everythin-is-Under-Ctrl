b1d2f3e6c075af3331fe667e8b9c0ecced9eb7b7
Trần Quang Khánh <131978279+sharksvn2804@users.noreply.github.com>
2025-12-18 18:15:41 +0700

    Refactor app_priv.h for improved organization

--- code/main/app_priv.h
+++ code/main/app_priv.h
@@ -5,19 +5,19 @@
 #include "driver/gpio.h"
 #include "driver/adc.h"
 
-// ================ MQ2 + GP2Y =================
+// ================= MQ2 + GP2Y + ADC =================
 #define MQ2_CHANNEL       ADC1_CHANNEL_0        // GPIO0
 #define PM25_ADC_CHANNEL  ADC1_CHANNEL_1        // GPIO1
 #define RL_VALUE          5000
 #define RATIO_CLEAN_AIR   3.0                   // Tỷ lệ Rs/R0 trong không khí sạch (được cung cấp bởi datasheet)
 
-// ==================== LED ====================
-// GPIO Pins:
+// ==================== GPIO + PWM ====================
+// Định nghĩa các chân GPIO:
 #define GP2Y_LED_POWER    19    // LED của GP2Y dùng để kích hoạt cảm biến bụi (dùng cho GP2Y1010AU0F)
 #define LED_MODE_PIN      2     // LED xanh - chế độ cảnh báo
 #define ALERT_BUTTON_PIN  18    // Nút GPIO18 để toggle alert mode từ button hoặc RainMaker
 #define BUZZ_PIN          6     // Buzz cảnh báo
-// GPIO cho PWM:
+// GPIO cho PWM LED cảnh báo:
 #define LED_WARNING_PIN   3     // LED đỏ 1 - cảnh báo khi CO hoặc PM2.5
 #define LEDC_CHANNEL      LEDC_CHANNEL_0
 #define LEDC_TIMER        LEDC_TIMER_0
